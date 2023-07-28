a2c7b5f1a4be58562847b323b4e8f56c2ceb82b3
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2023-07-28 17:02:13 +0545

    changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Navbar.css';
 
-import {BiLogoMediumOld} from 'react-icons/bi'
+import {BiLogoMediumOld} from 'react-icons/bi';
+import {AiFillCloseCircle} from 'react-icons/ai';
+import {PiDotsNineBold} from 'react-icons/pi';
+
 const Navbar = () => {
   return  <div className='navBar'>
         <div className="logoDiv">
@@ -10,23 +13,18 @@ const Navbar = () => {
         </div>
     <div className="menu">
 <ul>
-     <li className="navList">
-        Destination
- </li>
- <li className="navList">
-        About Us
- </li>
- <li className="navList">
-       Testimonial
- </li>
- <li className="navList">
-        Gallery
- </li>
+    <li className="navList"> Destination</li>
+    <li className="navList"> About Us</li>
+    <li className="navList">Testimonial</li>
+    <li className="navList">Gallery</li>
       </ul>
 
-      </div>
+      <AiFillCloseCircle className='icon'/>
+ </div>
+ <button className="signUpBtn btn">Sign Up</button>
     </div>;
   
+  <PiDotsNineBold className='icon'/>
 };
 
 export default Navbar;
