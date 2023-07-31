20c54e595f3b5cc0042f96b73a562841c1723f28
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2023-07-31 16:51:04 +0545

    changes

--- src/Components/Middle/Middle.jsx
+++ src/Components/Middle/Middle.jsx
@@ -2,8 +2,43 @@ import React from 'react';
 import './Middle.css';
 
 const Middle = () => {
-  return <div>
-      Middle
+  return <div className="middle section">
+    <div className="secContainer container">
+      <div className="grid">
+        <span className="flex">
+          <h1>10</h1>
+          <p>
+            World Of Experiences
+          </p>
+        </span>
+        <span className="flex">
+          <h1>10</h1>
+          <p>
+            World Of Experiences
+          </p>
+        </span>
+        <span className="flex">
+          <h1>2k+</h1>
+          <p>
+            Fine Destinations
+          </p>
+        </span>
+        <span className="flex">
+          <h1>10k+</h1>
+          <p>
+          Customer reviews
+          </p>
+        </span>
+
+        <span className="flex">
+          <h1>4.8</h1>
+          <p>
+           Overall Ratings
+          </p>
+        </span>
+      </div>
+
+    </div>
     </div>;
   
 };
