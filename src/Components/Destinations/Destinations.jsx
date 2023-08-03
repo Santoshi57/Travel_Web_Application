0a6d70a6b9bdb151c057c0a061dca68fc5a80421
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2023-08-03 14:21:45 +0545

    changes

--- src/Components/Destinations/Destinations.jsx
+++ src/Components/Destinations/Destinations.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 import './Destinations.css'
 
 
-import {MdLocationPin} from 'react-icons/md'
+
+
+//Imporetd icons
+import {MdLocationPin} from "react-icons/md"
+import {BsFillCreditCardFill} from "react-icons/bs"
+import {BsFillCalendarDateFill} from "react-icons/bs"
+import {BiSearchAlt} from 'react-icons/bi'
+import {TiLocation} from 'react-icons/ti'
+
+
+//impotred images
+import  image1 from '../../assets'
+
 const Destinations = () => {
   return (
     <div className='destination section container'>
@@ -23,7 +35,59 @@ const Destinations = () => {
       <MdLocationPin className='icon'/>
       <input type="text" placeholder='Location'/>
           </div>
+
+          <div className="inputField flex">
+            <BsFillCreditCardFill className="icon"/>
+            <input type="text" placeholder="Budget"/>
+          </div>
+
+          <div className="inputField flex">
+            <BsFillCalendarDateFill className="icon"/>
+            <input type="text" placeholder="Date"/>
+          </div>
+
+          <button className="btn flex"><BiSearchAlt className='icon'/>
+          Search </button>
         </div>
+
+
+        <div className="secMenu">
+          <ul className="flex">
+            <li className="active">All</li>
+            <li>Recommendeded</li>
+            <li>Beach</li>
+            <li>Park</li>
+            <li>Nature</li>
+            <li>Mountain</li>
+          </ul>
+
+        </div>
+
+        <div className="destinationContainer grid">
+          <div className="singleDestination">
+            <div className="imgDiv">
+            <img src={image1} alt="Destination Image "/>
+            
+            <div className="descInfo flex">
+              <div className="text">
+                <span className="name">
+                  Baro Baro Island
+                </span>
+                <p className ="flex">
+                  <TiLocation className="icon"/>
+                  Netherlands
+                  </p>
+              </div>
+              <span className="rating">
+                4.6
+              </span>
+            </div>
+            
+            </div>
+          </div>
+
+        </div>
+
       </div>
       
     </div>
