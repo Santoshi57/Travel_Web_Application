2a358d29c1b053fce8e012e7a235e18e7331854b
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2023-08-06 22:43:43 +0545

    changes

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 import './Reviews.css';
 
 const Reviews = () => {
-  return 
-    <div>
+  return  <div className="review section container">
+    <div className="secContainer grid">
+      <div className="textDiv">
+        <span className="redText">
+          FROM OUR CLIENTS
+        </span>
+        <h3>
+          Real Travel History From our Beloved Clients
+        </h3>
+        <p>
+          By choosng us as their tour agency, customers can expect an enriching and enjoyable travel experience,
+          filled with unforgettable
+          memories that will last a lifetime.
+        </p>
+      </div>
+    </div>
       
     </div>;
   
