2772fbfc725e8713696dd97562b2ca6cdb21f58e
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2023-08-07 16:26:32 +0545

    changes

--- src/Components/Questions/Questions.jsx
+++ src/Components/Questions/Questions.jsx
@@ -2,9 +2,28 @@ import React from 'react';
 import './Questions.css';
 
 const Questions = () => {
-  return 
-    <div>
-      
+  return  <div className="questions section container">
+      <div className="secHeading">
+       <h3>Frequently Asked Questions</h3> 
+      </div>
+      <div className="secContainer grid">
+        <div className="accordion grid">
+
+        </div>
+        <div className="form">
+          <div className="secHeading">
+            <h4>DO you have any specific questions?</h4>
+            <p>
+              Please fill the form below and our dedicated team will get in touch with you as soon as possible.
+            </p>
+          </div>
+          <div className="formContent grid">
+            <input type="email" placeholder="Enter email address"/>
+            <textarea placholder="Enter your question here"></textarea>
+            <button className="btn">Submit Inquiry</button>
+          </div>
+        </div>
+      </div>
     </div>;
   
 };
