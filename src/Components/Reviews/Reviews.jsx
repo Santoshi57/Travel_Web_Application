223fd09256e758278ba6f7af8ae594977a4371fd
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2023-08-07 15:06:22 +0545

    changes

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './Reviews.css';
 
+
+import {AiFillStar} from 'react-icons/ai'
+
+import Image1 from "../../assets/image1.jpeg";
+import Image2 from "../../assets/image1.jpeg";
+import Image3 from "../../assets/image1.jpeg";
+import Image4 from "../../assets/image1.jpeg";
+
+
+
 const Reviews = () => {
   return  <div className="review section container">
     <div className="secContainer grid">
@@ -16,6 +26,25 @@ const Reviews = () => {
           filled with unforgettable
           memories that will last a lifetime.
         </p>
+        <span className="stars flex">
+          <AiFillStar className ="icon"/>
+          <AiFillStar className ="icon"/>
+          <AiFillStar className ="icon"/>
+          <AiFillStar className ="icon"/>
+          <AiFillStar className ="icon"/>
+        </span>
+
+<div className="clientsImages flex">
+  <img src={Image1} alt=" Client Image"/>
+  <img src={Image2} alt=" Client Image"/>
+  <img src={Image3} alt=" Client Image"/>
+  <img src={Image4} alt=" Client Image"/>
+
+</div>
+      </div>
+
+      <div className="imgDiv">
+        <img src={image1} alt="Div image"/>
       </div>
     </div>
       
