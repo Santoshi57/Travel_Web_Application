f806f723b4625b77eb7d5d55aec5b64c59ae9e47
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2023-08-08 23:17:59 +0545

    changes

--- src/Components/Questions/Accordion.jsx
+++ src/Components/Questions/Accordion.jsx
@@ -5,14 +5,14 @@ import{BsArrowDownCircle} from 'react-icons/bs';
 
 const Accordion = () => {
   return (
-    <div className ='accordionContainer'>
+    <div className ="accordionContainer">
         <span className="title flex ">
            How do i choose the right travel destination for me?
           <span>
-          <BsArrowDownCircle className='icon'/> 
+          <BsArrowDownCircle className="icon"/> 
           </span> 
         </span>
-  <p>
+  <p className="description">
     Consider your interests, budget,desired experiences,and the type of environment you 
     enjoy. Research destinations that allign with your preferences and offer attractions or activities you 
     find appealing.
